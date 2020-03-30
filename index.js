dcb0cb65433b5b2403a6500d033a13edc53ba8ee
ETHproductions <ethprod@mail.com>
2020-03-30 14:13:40 -0400

    Make result section dynamic with Vue
    Doesn't respond to input yet, but it's halfway there

--- index.js
+++ index.js
@@ -1,3 +1,203 @@
+Vue.component('tier-marker', {
+    props: {
+        tier: Number, // default tier on the course: 1 for bottom, 2 for middle, 3 for top
+        boost: String // boost received, if any: "cup", "spotlight", or "cup & spotlight"
+    },
+    data: function () {
+        let tiers = [, "bottom", "middle", "top"];
+        let tierboost = this.boost ? this.boost.length > 10 ? 2 : 1 : 0;
+        let fulltier = Math.min(+this.tier + tierboost, 3);
+        let tiertext = fulltier == 3 ? 'top' : fulltier == 2 ? 'middle' : 'bottom';
+        let title = `This item is ${ tiers[this.tier] }-tier on this course`;
+        if (this.tier != fulltier)
+            title += `, but receives a ${ this.boost } boost to become ${ tiers[fulltier] }-tier`;
+        return {
+            title: title,
+            text: "+".repeat(fulltier)
+        }
+    },
+    template: '<span :title="title">{{ text }}</span>'
+});
+
+Vue.component('dkg-header', {
+    props: {
+        object: Object
+    },
+    template: '<tr><th colspan=2>{{ object.name }} <tier-marker :tier=object.tier :boost=object.tierboost></tier-marker></th></tr>'
+});
+
+Vue.component('dkg-prop', {
+    props: {
+        name: String,
+        value: String
+    },
+    template: `
+      <tr>
+        <td class="td-l">{{ name }}</td>
+        <td class="td-r">{{ value }}</td>
+      </tr>`
+});
+
+Vue.component('infobox', {
+    props: {
+        object: Object // the object representing the driver, kart, or glider
+    },
+    template: `
+      <table class="infobox">
+        <tbody>
+          <tr is="dkg-header" :object="object"></tr>
+          <tr is="dkg-prop" v-for="(value, name) in object.displayprops" :key="name" :name="name" :value="value"></tr>
+        </tbody>
+      </table>`
+})
+
+Vue.component('imagebox', {
+    props: {
+        type: String,
+        name: String,
+        rarity: String,
+        skill: String
+    },
+    data: function () {
+        return {
+            mainimg: `img/${ this.type }s/${ this.name }.png`,
+            rarityimg: `img/items/${ this.rarity }.png`,
+            skillimg: `img/items/${ this.skill }.png`,
+            skilltitle: this.type == 'driver' ? this.skill : this.skill + ' Plus'
+        }
+    },
+    template: `
+    <div class="result-dkg-img">
+      <img :src="mainimg" :title="name">
+      <img class="icon" :src="rarityimg" :title="rarity">
+      <img class="icon" :src="skillimg" :title="skilltitle">
+    </div>`
+});
+
+Vue.component('result-dkg', {
+    props: {
+        object: Object
+    },
+    template:`
+      <div :class="'result-dkg result-' + object.type">
+        <imagebox v-bind="object"></imagebox>
+        <table is="infobox" :object="object"></table>
+      </div>`
+});
+
+Vue.component('result-box', {
+    props: {
+        objects: Array
+    },
+    template:`
+      <div class="result">
+        <result-dkg v-for="object in objects" :object="object"></result-dkg>
+      </div>`
+});
+
+var app = new Vue({
+    el: '#container',
+    data: {
+        setup1: [
+            {
+                type: "driver",
+                name: "Hammer Bro",
+                rarity: "Super",
+                skill: "Hammer",
+                tier: 2,
+                tierboost: "spotlight",
+                displayprops: {
+                    "Level": "3",
+                    "Items": "3",
+                    "Frenzy chance": "+3%",
+                    "Base points": "657",
+                    "Bonus-points boost": "+6.57"
+                }
+            },
+            {
+                type: "kart",
+                name: "Quickshaw",
+                rarity: "High-End",
+                skill: "Rocket Start",
+                tier: 2,
+                tierboost: "",
+                displayprops: {
+                    "Level": "4",
+                    "Multiplier": "×1.8",
+                    "Rocket Start bonus": "+80",
+                    "Base points": "400",
+                    "Bonus-points boost": "none"
+                }
+            },
+            {
+                type: "glider",
+                name: "Super Glider",
+                rarity: "Normal",
+                skill: "Green Shell",
+                tier: 3,
+                tierboost: "",
+                displayprops: {
+                    "Level": "4",
+                    "Combo time": "×2.2",
+                    "Combo bonus": "×3",
+                    "Green Shell bonus": "+10",
+                    "Base points": "300",
+                    "Bonus-points boost": "+4.5"
+                }
+            }
+        ],
+        setup2: [
+            {
+                type: "driver",
+                name: "Black Shy Guy",
+                rarity: "Super",
+                skill: "Bob-omb Cannon",
+                tier: 3,
+                tierboost: "",
+                displayprops: {
+                    "Level": "3",
+                    "Items": "3",
+                    "Frenzy chance": "+3%",
+                    "Base points": "657",
+                    "Bonus-points boost": "+6.57"
+                }
+            },
+            {
+                type: "kart",
+                name: "Warship",
+                rarity: "Normal",
+                skill: "Mini-Turbo",
+                tier: 3,
+                tierboost: "",
+                displayprops: {
+                    "Level": "1",
+                    "Multiplier": "×2.0",
+                    "Mini-Turbo bonus": "+2",
+                    "Base points": "300",
+                    "Bonus-points boost": "+0"
+                }
+            },
+            {
+                type: "glider",
+                name: "Super Glider",
+                rarity: "Normal",
+                skill: "Green Shell",
+                tier: 3,
+                tierboost: "",
+                displayprops: {
+                    "Level": "4",
+                    "Combo time": "×2.2",
+                    "Combo bonus": "×3",
+                    "Green Shell bonus": "+10",
+                    "Base points": "300",
+                    "Bonus-points boost": "+4.5"
+                }
+            }
+        ]
+    }
+});
+
+
 function fetchLocal(url)
 {
     return new Promise((resolve, reject) => {
